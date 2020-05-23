c5644c4e496507091c3864d414265f2e0a515ac2
Margo <margo.lee.smith@gmail.com>
2020-05-23 13:44:23 -0700

    enable 4825 mission

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const screensavers = [
   "https://cdn.jsdelivr.net/gh/margo73465/squares/squares.js",
   "https://cdn.jsdelivr.net/gh/margo73465/triangles/triangles.js",
   //"https://cdn.jsdelivr.net/gh/margo73465/stars/margo-party.js",
-  //"https://cdn.jsdelivr.net/gh/margo73465/4825-mission/main.js",
+  "https://cdn.jsdelivr.net/gh/margo73465/4825-mission/main.js",
 ];
 const randomNumber = Math.floor(Math.random() * screensavers.length);
 const currentScreensaver = screensavers[randomNumber];
