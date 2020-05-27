adece2e8a5a2f78405052e31b6cf6bf11a7283d6
Margo <margo.lee.smith@gmail.com>
2020-05-26 18:11:47 -0700

    use styles as well as javascript from other projects (this fixes 4825-mission)

--- main.js
+++ main.js
@@ -1,18 +1,47 @@
-console.log( `Hi! Thanks for checking out my website :).
+console.log( `Hi, thanks for checking out my website!
   The source code is all un-minified in case you want to
   see how anything works. Let me know if you have any
   questions or comments!`);
 
 const screensavers = [
-  "https://cdn.jsdelivr.net/gh/margo73465/squares/squares.js",
-  "https://cdn.jsdelivr.net/gh/margo73465/triangles/triangles.js",
-  //"https://cdn.jsdelivr.net/gh/margo73465/stars/margo-party.js",
-  "https://cdn.jsdelivr.net/gh/margo73465/4825-mission/main.js",
+  {
+    "name": "squares",
+    "js": "https://cdn.jsdelivr.net/gh/margo73465/squares/squares.js"
+  },
+  {
+    "name": "triangles",
+    "js": "https://cdn.jsdelivr.net/gh/margo73465/triangles/triangles.js"
+  },
+  //{
+  //  "name": "stars",
+  //  "js": "https://cdn.jsdelivr.net/gh/margo73465/stars/margo-party.js",
+  //  "css": "https://cdn.jsdelivr.net/gh/margo73465/stars/styles.css"
+  //},
+  {
+    "name": "mural",
+    "js": "https://cdn.jsdelivr.net/gh/margo73465/4825-mission/main.js",
+    "css": "https://cdn.jsdelivr.net/gh/margo73465/4825-mission/styles.css"
+  }
 ];
+
 const randomNumber = Math.floor(Math.random() * screensavers.length);
 const currentScreensaver = screensavers[randomNumber];
-const screensaverScript = document.createElement("script");
-screensaverScript.setAttribute("type", "text/javascript");
-screensaverScript.setAttribute("src", currentScreensaver);
+
 const svgElement = document.getElementById("svg");
-document.body.insertBefore(screensaverScript, svgElement);
+
+if (currentScreensaver.js) {
+  const screensaverScript = document.createElement("script");
+  screensaverScript.setAttribute("type", "text/javascript");
+  screensaverScript.setAttribute("src", currentScreensaver.js);
+  document.body.insertBefore(screensaverScript, svgElement);
+}
+
+if (currentScreensaver.css) {
+  const screensaverStyles = document.createElement("link");
+  screensaverStyles.setAttribute("type", "text/css");
+  screensaverStyles.setAttribute("rel", "stylesheet");
+  screensaverStyles.setAttribute("href", currentScreensaver.css);
+  const existingStylesheet = document.getElementsByTagName("link")[0];
+  document.body.insertBefore(screensaverStyles, svgElement);
+}
+
